fd6e541d53ebebb44686cb7f53d8ada5c6a4ed7e
achikoma <a-ono@ono-n.jp>
2023-09-27 15:52:34 +0900

    JavaScript 学習レッスン IVレッスン7の変更

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
 class Animal {
-  // 引数に「name」と「age」を追加してください
-  constructor(name,age) {
-    // 「"レオ"」の代わりに引数nameの値を代入してください
+  constructor(name, age) {
     this.name = name;
-    
-    // 「3」の代わりに引数ageの値を代入してください
     this.age = age;
   }
+  
+  // greetメソッドを追加してください
+  greet(){
+    console.log("こんにちは");
+  }
+  
 }
 
-// 引数に「"モカ"」と「8」を渡してください
-const animal = new Animal("モカ",8);
+const animal = new Animal("レオ", 3);
 
 console.log(`名前: ${animal.name}`);
 console.log(`年齢: ${animal.age}`);
+
+// animalに対してgreetメソッドを呼び出してください
+animal.greet();
