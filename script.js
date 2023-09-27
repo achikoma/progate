671f1e11b6af70c1c9b0cfa70aea898c30a81877
achikoma <a-ono@ono-n.jp>
2023-09-27 10:12:49 +0900

    JavaScript 学習レッスン II レッスン8の変更

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const animals = ["dog", "cat", "sheep"];
 
-// 配列animalsの3つ目の要素を「rabbit」に更新してください
-animals[2] = "rabbit";
-
-// 配列animalsの3つ目の要素をコンソールに表示して下さい
-console.log(animals[2]);
+// for文を用いて、配列の値を順にコンソールに出力してください
+for (let i = 0; i < 3; i++) {
+  console.log(animals[i]);
+}
