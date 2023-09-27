48eed18cfa830977706aa778380d30b736f822eb
achikoma <a-ono@ono-n.jp>
2023-09-27 11:13:55 +0900

    JavaScript 学習レッスン II レッスン16の変更

--- script.js
+++ script.js
@@ -1,21 +1,16 @@
-const characters = [
-  { name: "にんじゃわんこ", age: 14 },
-  { name: "ひつじ仙人", age: 100 },
-  { name: "ベイビーわんこ", age: 5 },
-  { name: "とりずきん" },
-];
+const cafe = {
+  name: "Progateカフェ",
+  businessHours: {
+    // businessHoursの値に指定されたオブジェクトを代入してください
+    opening: "10:00(AM)",
+    closing: "8:00(PM)",
+  },
+};
 
-for (let i = 0; i < characters.length; i++) {
-  console.log("--------------------");
+// 「店名:〇〇」を出力してください
+console.log(`店名:${cafe.name}`);
 
-  const character = characters[i];
-
-  console.log(`名前は${character.name}です`);
-
-  // if文を追加してください
-  if (character.age == undefined) {
-    console.log("年齢は秘密です");
-  } else {
-    console.log(`${character.age}歳です`);
-  }
-}
+// 「営業時間:〇〇から△△」を出力してください
+console.log(
+  `営業時間:${cafe.businessHours.opening}から${cafe.businessHours.closing}`
+);
