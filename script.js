f581b75f4fd528d3ca5b4b0872911d54b34c04b3
achikoma <a-ono@ono-n.jp>
2023-09-27 13:34:54 +0900

    JavaScript 学習レッスン III レッスン1の変更

--- script.js
+++ script.js
@@ -1,16 +1,7 @@
-const cafe = {
-  name: "Progateカフェ",
-  businessHours: {
-    // businessHoursの値に指定されたオブジェクトを代入してください
-    opening: "10:00(AM)",
-    closing: "8:00(PM)",
-  },
-};
+const greet = function () {
+  console.log('こんにちは！')
+  console.log('関数を学習していきましょう！')
+}
 
-// 「店名:〇〇」を出力してください
-console.log(`店名:${cafe.name}`);
-
-// 「営業時間:〇〇から△△」を出力してください
-console.log(
-  `営業時間:${cafe.businessHours.opening}から${cafe.businessHours.closing}`
-);
+// 関数を呼び出してください
+greet();
