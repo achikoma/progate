662e40347872e74b8ca140214c679413b1f54984
achikoma <a-ono@ono-n.jp>
2023-09-27 17:10:49 +0900

    JavaScript 学習レッスン IVレッスン13の変更

--- script.js
+++ script.js
@@ -16,18 +16,21 @@ class Animal {
 }
 
 class Dog extends Animal {
-  // getHumanAgeメソッドを追加してください
-  getHumanAge(){
-    return this.age * 7;
+  // infoメソッドを追加してください
+  info(){
+    this.greet();
+    console.log(`名前は${this.name}です`);
+    console.log(`${this.age}歳です`);
+    
+    const humanAge = this.getHumanAge();
+    console.log(`人間年齢で${humanAge}歳です`);
   }
 
+  getHumanAge() {
+    return this.age * 7;
+  }
 }
 
 const dog = new Dog("レオ", 4);
 dog.info();
 
-// 定数humanAgeを定義し、定数dogに対してgetHumanAgeメソッドを呼び出した値を代入してください
-const humanAge = dog.getHumanAge();
-
-// 「人間年齢で〇〇歳です」と出力してください
-console.log(`人間年齢で${humanAge}歳です`);
