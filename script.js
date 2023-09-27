6d9c07e7a8ee4c8d5c60318f14f9211468b096ca
achikoma <a-ono@ono-n.jp>
2023-09-27 14:31:51 +0900

    JavaScript 学習レッスン III レッスン4の変更

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-// 定数greetにアロー関数を代入してください
-const greet = () => {
-  console.log('こんにちは！');
-}
+// 関数の引数にnumber1とnumber2を追加してください
+const add = (number1,number2) => {
+  // number1とnumber2を足した値をコンソールに出力してください
+  console.log(number1+number2);
+  
+};
 
-// 定数greetを呼び出してください
-greet();
+// 引数に5と7を渡して関数を呼び出してください
+add(5,7);
