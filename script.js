cbee37f397dee3b89afdc740aea73d5e36cc1de8
achikoma <a-ono@ono-n.jp>
2023-09-28 10:01:05 +0900

    JavaScript 学習レッスン Vレッスン1の変更

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-// call constructor, new object, assign it to bike
-const bike = new Bike();
-//Change internal state by calling a function
-bike.changeGear("front", "Up");
-// add a new member to bike that did not exist before
-bike.type = "Penny Farthing";
-// check for success
-console.log(bike.calculateGearRatio()); // 4.0909...
-console.log(bike.type); // "Penny Farthing"
-// attempt to point bike to new instance of Bike
-bike = new Bike(1,2); // error
+// 以下はAnimalクラス, Dogクラスの定義部分ではないので残してください
+const dog = new Dog("レオ", 4, "チワワ");
+dog.info();
