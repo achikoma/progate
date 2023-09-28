01b3107d5afdd8c5471fa1478c9fe35b0ff42cef
achikoma <a-ono@ono-n.jp>
2023-09-28 17:19:30 +0900

    JavaScript 学習レッスン VI レッスン1の変更

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-import { dog1, dog2 } from "./data/dogData";
+const characters = ["にんじゃわんこ", "ベイビーわんこ", "ひつじ仙人"];
 
-console.log("---------");
-dog1.info();
-console.log("---------");
-dog2.info();
+console.log(characters);
+
+// pushメソッドを使って配列charactersに、文字列「とりずきん」を追加してください
+characters.push("とりずきん");
+
+// 配列charactersを出力してください
+console.log(characters);
